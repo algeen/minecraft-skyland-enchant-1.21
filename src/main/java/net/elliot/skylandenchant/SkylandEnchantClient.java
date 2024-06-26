21d1f4944b293d707cf7c955933303474e6bc910
elliot <elliot.erhagen@gmail.com>
2024-06-26 16:48:54 +0200

    test igen

---
+++ src/main/java/net/elliot/skylandenchant/SkylandEnchantClient.java
@@ -0,0 +1,10 @@
+package net.elliot.skylandenchant;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkylandEnchantClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/net/elliot/skylandenchant/SkylandEnchantDataGenerator.java
@@ -0,0 +1,11 @@
+package net.elliot.skylandenchant;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkylandEnchantDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
